8251a883ce69b3a053baa1904a2efb2292b5dc6f
nagipragalathan <nagipragalathan@gmail.com>
2024-09-30 10:50:43 +0530

    "Refactored code to wrap id_to_content call in clg_info_top_details function and added function call"

--- TabExractions/ClgInfo.py
+++ TabExractions/ClgInfo.py
@@ -2,13 +2,19 @@ from Tools.Tool import id_to_content, driver
 
 
 url = 'https://www.shiksha.com/college/adina-institute-of-science-and-technology-sagar-60309'
-data_dict = [url, {'line_1': 'e9dd86', 'line_2': 'e1a898'}]
-
-result = id_to_content(data_dict)
-print(result)
 
 
 
+def clg_info_top_details(url):
+    data_dict = [url, {'line_1': 'e9dd86', 'line_2': 'e1a898'}]
+    result = id_to_content(data_dict)
+    return result
 
-# To Close the driver : )
+
+print(clg_info_top_details(url))
+
+
+
+
+# Dont remove this line 🙂... To Close the driver : )
 driver.quit()
